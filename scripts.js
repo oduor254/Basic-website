edd0b8a915111e5976e39c1aec7c30a65982fbcc
oduor254 <140979885+oduor254@users.noreply.github.com>
2023-07-30 23:15:45 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Function to handle form submission
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // You can add your own logic to handle the form submission here
+    alert("Thank you! Your message has been submitted.");
+});
